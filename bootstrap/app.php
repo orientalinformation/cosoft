42d9645534d56ca119ea1310520a1c7ab33eead2
Thao Le <thaolt@songphi.com>
2017-11-25 17:55:29 +0700

    added model generator for local env

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,6 +81,9 @@ $app->singleton(
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+if ($app->environment('local')) {
+    $app->register(Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class);
+}
 
 /*
 |--------------------------------------------------------------------------
