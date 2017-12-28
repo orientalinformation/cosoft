efba41ed208be66a4bb4df28779082bc78e5458a
Thao Le <thaolt@songphi.com>
2017-12-28 13:15:27 +0700

    service for updating study & production

--- routes/api_thaolt.php
+++ routes/api_thaolt.php
@@ -7,3 +7,19 @@
  * Output-Formats: [application/json]
  */
 $router->GET('/api/v1/products/{id}/meshView', 'Api1\\Products@getMeshView');
+
+/**
+ * PATCH saveStudy
+ * Summary:
+ * Notes:
+ * Output-Formats: [application/json]
+ */
+$router->PATCH('/api/v1/studies/{id}', 'Api1\\Studies@saveStudy');
+
+/**
+ * PATCH saveProduction
+ * Summary:
+ * Notes:
+ * Output-Formats: [application/json]
+ */
+$router->PATCH('/api/v1/productions/{id}', 'Api1\\Productions@saveProduction');
