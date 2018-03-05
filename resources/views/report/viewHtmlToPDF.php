e9b766bcb83ae4916dc1b33d498788e2c97b445b
Huy Tran <huytd@dfm-engineering.com>
2018-03-05 09:46:26 +0700

    resource view for report

---
+++ resources/views/report/viewHtmlToPDF.php
@@ -0,0 +1,11 @@
+
+<h1>Generate a PDF using TCPDF in laravel </h1>
+<h4>by<br/>Learn Infinity</h4>
+<table border="1" width="100%" cellpadding="10" >
+	<tr>
+		<th width="10%">SNo.</th>
+		<th width="40%">Name</th>
+		<th width="50%">Email</th>
+	</tr>
+ 
+</table>
---
+++ resources/views/report/view_report.php
@@ -0,0 +1,87 @@
+<html>
+    <body>
+        <div>
+            <div class="table table-bordered">
+                <table border="0.5">
+                <tr>
+                    <th>Daily production</th>
+                    <th align="center">$var</th>
+                    <th>Hours/Day</th>
+                </tr>
+                <tr>
+                    <td>Weekly production</td>
+                    <td align="center">$var</td>
+                    <td>Days/Week</td>
+                </tr>
+                <tr style="height: 10px;">
+                    <td>Annual production</td>
+                    <td align="center">$var</td>
+                    <td>Weeks/Year</td>
+                </tr>
+                <tr>
+                    <td>Number of equipment cooldowns</td>
+                    <td align="center">$var</td>
+                    <td>per day</td>
+                </tr>
+                <tr>
+                    <td>Factory Air temperature</td>
+                    <td align="center">$var</td>
+                    <td>(°C)</td>
+                </tr>
+                <tr>
+                    <td>Relative Humidity of Factory Air</td>
+                    <td align="center">$var</td>
+                    <td>(%)</td>
+                </tr>
+                <tr>
+                    <td>Required Average temperature</td>
+                    <td align="center">$var</td>
+                    <td>(°C)</td>
+                </tr>
+                <tr>
+                    <td>Required Production Rate</td>
+                    <td align="center">$var</td>
+                    <td>(kg/h)</td>
+                </tr>
+                </table>
+            </div>
+        </div>
+        <div>
+            <div align="center">
+                <img src="'.$tcpdf_path .'/images/banner_cryosoft.png" height="400" width="4200">
+            </div>
+            <div><strong><u>Customer:</u></strong></div>
+                <div><strong>Company name : '. PDF::SetTitle("Cryosoft Report") .' </strong></div>
+                <div><strong>Surname / Name :</strong></div>
+                <div><strong>Function :</strong></div>
+                <div><strong>Contact :</strong></div>
+                <div><strong>Date of the redivort generation : 27/02/2018 </strong></div>
+            <div align="center">
+                <img src="'.$tcpdf_path .'/images/image_cryosoft.png" height="200" width="200">
+            </div>
+            <div class="table table-bordered">
+                <table border="1">
+                <tr>
+                    <th align="center" colspan="2">Study of the product: AAK STUDY 1 23 FEB</th>
+                </tr>
+                <tr>
+                    <td >Calculation mode :</td>
+                    <td align="center">Optimum equipment</td>
+                </tr>
+                <tr>
+                    <td >Economic :</td>
+                    <td align="center">NO</td>
+                </tr>
+                <tr>
+                    <td >Cryogenic Pipeline :</td>
+                    <td align="center">NO</td>
+                </tr>
+                <tr>
+                    <td >Chaining :</td>
+                    <td align="center">YES</td>
+                </tr>
+                </table>
+            </div>
+        </div>
+    </body>
+</html>
