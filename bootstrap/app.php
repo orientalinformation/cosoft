431cac43a2c4da061e35ef47c9f44ad94ae3f632
Hai Do <haidt@dfm-engineering.com>
2018-03-14 11:40:15 +0700

    fix MediaUpload

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -114,6 +114,9 @@ $app->register(Elibyy\TCPDF\ServiceProvider::class);
 
 class_alias('\Elibyy\TCPDF\Facades\TCPDF', 'PDF');
 
+$app->register(Plank\Mediable\MediableServiceProvider::class);
+class_alias(Plank\Mediable\MediaUploaderFacade::class, 'MediaUploader');
+
 if ($app->environment('local')) {
     $app->register(Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class);
     $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
