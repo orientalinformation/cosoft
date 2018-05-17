447e6bb1c51d5e72676c01fed5c266b9b9ab5471
ngonc <ngonc@dfm-engineering.com>
2018-05-17 19:42:50 +0700

    Task #1005 fix equiplayout

--- app/Cryosoft/StudyEquipmentService.php
+++ app/Cryosoft/StudyEquipmentService.php
@@ -816,11 +816,15 @@ class StudyEquipmentService
         // We find back the product length and width from other values
         $plength = 0.0; // double
         $pwidth = 0.0; // double
- 
-        $pwidth = ($lfEquipWidth - 2 * $layoutRes->LEFT_RIGHT_INTERVAL
+        
+        if (($layoutRes->NUMBER_IN_WIDTH != 0)) {
+            $pwidth = ($lfEquipWidth - 2 * $layoutRes->LEFT_RIGHT_INTERVAL
             - $widthInterVal * ($layoutRes->NUMBER_IN_WIDTH - 1)) / $layoutRes->NUMBER_IN_WIDTH;
-
-        $plength = $lfEquipLength / $layoutRes->NUMBER_PER_M - $lengthInterVal;
+        }
+        
+        if (($layoutRes->NUMBER_PER_M - $lengthInterVal != 0)) {
+            $plength = $lfEquipLength / $layoutRes->NUMBER_PER_M - $lengthInterVal;
+        }
 
         if ($sequip->BATCH_PROCESS) {
             //convert
