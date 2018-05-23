88341ab74b7d4572801f776204846bae07f2e240
Dong Tran <dongtp@dfm-engineering.com>
2018-05-23 10:35:23 +0700

    Add controller checkWarnings

---
+++ app/Http/Controllers/Api1/CheckWarnings.php
@@ -0,0 +1,39 @@
+<?php
+/****************************************************************************
+ **
+ ** Copyright (C) 2017 Oriental Tran.
+ ** Contact: dongtp@dfm-engineering.com
+ ** Company: DFM-Engineering Vietnam
+ **
+ ** This file is part of the cryosoft project.
+ **
+ **All rights reserved.
+ ****************************************************************************/
+namespace App\Http\Controllers\Api1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Equipment;
+use App\Models\Study;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Auth\Factory as Auth;
+use App\Models\StudyEquipment;
+
+class CheckWarnings extends Controller 
+{
+	protected $request;
+    protected $auth;
+
+    public function __construct(\Laravel\Lumen\Application $app)
+    {
+        $this->app = $app;
+        $this->auth = $app['Illuminate\\Contracts\\Auth\\Factory'];
+        $this->request = $app['Illuminate\\Http\\Request'];
+    }
+
+    public function checkWarningEquipment()
+    {
+    	$message = null;
+    	
+    	return $message;
+    }
+}
