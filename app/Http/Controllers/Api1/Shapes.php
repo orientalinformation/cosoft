c2458b6568e9eaa23157313cf8dde27504daf760
Dong Tran <dongtp@dfm-engineering.com>
2018-06-11 14:50:12 +0700

    add filter calmode shapre

--- app/Http/Controllers/Api1/Shapes.php
+++ app/Http/Controllers/Api1/Shapes.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\Auth\Factory as Auth;
+use App\Models\Shape;
 
 class Shapes extends Controller
 {
@@ -33,13 +34,22 @@ class Shapes extends Controller
 
     public function getShapes()
     {
-        $shapes = \App\Models\Shape::all()->toArray();
+        $input = $this->request->all();
+
+        if (isset($input['calMode'])) $calMode = intval($input['calMode']);
+
+        $shapes = Shape::all()->toArray();
+        if ($calMode == 0) {
+            $shapes = Shape::where('ID_SHAPE', '<', 10)->get();
+        } else {
+            $shapes = Shape::where('ID_SHAPE', '>=', 10)->get();
+        }
         return $shapes;
     }
 
     public function getShapeById($id)
     {
-        $shape = \App\Models\Shape::find($id);
+        $shape = Shape::find($id);
         return $shape;
     }
 }
