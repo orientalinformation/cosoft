669b3907e696f3dfcb66a894fdb24073e911f1a2
Oriental Tran <dongtp@dfm-engineering.com>
2018-07-04 14:32:53 +0700

    Add new layput eclipse

--- legacy/com/oxymel/ofc/conveyer/Product.php
+++ legacy/com/oxymel/ofc/conveyer/Product.php
@@ -5,20 +5,32 @@ use java\lang\StringBuffer;
 
 class Product 
 {
-    protected $_height;	// double
-    protected $_width;	// double
-    protected $_svgHeight;	// double
-    protected $_svgWidth;	// double
-    protected $_shape;	// short
-    public static $SLAB;	// short
-    public static $PARALLELEPIPED_STANDING;	// short
-    public static $PARALLELEPIPED_LAYING;	// short
-    public static $CYLINDER_STANDING;	// short
-    public static $CYLINDER_LAYING;	// short
-    public static $SPHERE;	// short
-    public static $CYLINDER_CONCENTRIC_STANDING;	// short
-    public static $CYLINDER_CONCENTRIC_LAYING;	// short
-    public static $PARALLELEPIPED_BREADED;	// short
+    protected $_height; // double
+    protected $_width;  // double
+    protected $_svgHeight;  // double
+    protected $_svgWidth;   // double
+    protected $_shape;
+    public static $SLAB;
+    public static $PARALLELEPIPED_STANDING;
+    public static $PARALLELEPIPED_LAYING;
+    public static $CYLINDER_STANDING;
+    public static $CYLINDER_LAYING;
+    public static $SPHERE;
+    public static $CYLINDER_CONCENTRIC_STANDING;
+    public static $CYLINDER_CONCENTRIC_LAYING;
+    public static $PARALLELEPIPED_BREADE;
+    public static $PARALLELEPIPED_BREADED;
+    public static $D_RECTANGULAR_BLOCK_V;
+    public static $D_RECTANGULAR_BLOCK_H;
+    public static $D_STANDING_CYLINDER;
+    public static $D_LYLING_CYLINDER;
+    public static $D_SPHERE;
+    public static $D_STANDING_CONCENTRIC_CYLINDER;
+    public static $D_LYLING_CONCENTRIC_CYLINDER;
+    public static $D_RECTANGULAR_BLOCK_B;
+    public static $D_TRAPEZOIND_3D;
+    public static $D_STANDING_OVAL;
+    public static $D_LYLING_OVAL;
 
     // default class members
     private function __init()
@@ -39,6 +51,17 @@ class Product
         self::$CYLINDER_CONCENTRIC_STANDING = 7;
         self::$CYLINDER_CONCENTRIC_LAYING = 8;
         self::$PARALLELEPIPED_BREADED = 9;
+        self::$D_RECTANGULAR_BLOCK_V = 10;
+        self::$D_RECTANGULAR_BLOCK_H = 11;
+        self::$D_STANDING_CYLINDER = 12;
+        self::$D_LYLING_OVAL = 13;
+        self::$D_SPHERE = 14;
+        self::$D_STANDING_CONCENTRIC_CYLINDER = 15;
+        self::$D_LYLING_CONCENTRIC_CYLINDER = 16;
+        self::$D_RECTANGULAR_BLOCK_B = 17;
+        self::$D_TRAPEZOIND_3D = 18;
+        self::$D_STANDING_OVAL = 19;
+        self::$D_LYLING_OVAL = 20;
     }
 
     // [double height, double width, short shape]
@@ -91,16 +114,28 @@ class Product
             case self::$PARALLELEPIPED_BREADED:
             case self::$CYLINDER_LAYING:
             case self::$CYLINDER_CONCENTRIC_LAYING:
-                // var_dump($this); die('asw');
+                $tmp->append((((((((("\t<rect x=\"" . $x) . "\" y=\"") . $y) . "\" width=\"") . $this->_svgWidth) . "\" height=\"") . $this->_svgHeight) . "\" fill=\"gray\" stroke=\"black\" stroke-width=\"1\"/>\n"));
+                break;
+            case self::$D_LYLING_OVAL:
                 $tmp->append((((((((("\t<rect x=\"" . $x) . "\" y=\"") . $y) . "\" width=\"") . $this->_svgWidth) . "\" height=\"") . $this->_svgHeight) . "\" fill=\"gray\" stroke=\"black\" stroke-width=\"1\"/>\n"));
                 break;
             case self::$CYLINDER_STANDING:
             case self::$CYLINDER_CONCENTRIC_STANDING:
+            case self::$D_STANDING_CONCENTRIC_CYLINDER:
+            case self::$D_LYLING_CONCENTRIC_CYLINDER:
             case self::$SPHERE:
+            case self::$D_SPHERE:
                 $x += ($this->_svgWidth / 2);
                 $y += ($this->_svgHeight / 2);
                 $r = ($this->_svgWidth / 2);
                 $tmp->append((((((("\t<circle cx=\"" . $x) . "\" cy=\"") . $y) . "\" r=\"") . $r) . "\" fill=\"gray\" stroke=\"black\" stroke-width=\"1\"/>\n"));
+                break;
+            case self::$D_STANDING_OVAL:
+                $x += ($this->_svgWidth / 2);
+                $y += ($this->_svgHeight / 2);
+                $r = ($this->_svgWidth / 2);
+                $ry = $r / 2;
+                $tmp->append((((((((("\t<ellipse cx=\"" . $x) . "\" cy=\"") . $y) . "\" rx=\"") . $r) . "\" ry=\"") . $ry). "\" fill=\"gray\" stroke=\"black\" stroke-width=\"1\"/>\n"));
                 break;
             default:
                 $tmp->append((((((((("\t<rect x=\"" . $x) . "\" y=\"") . $y) . "\" width=\"") . $this->_svgWidth) . "\" height=\"") . $this->_svgHeight) . "\" fill=\"gray\" stroke=\"black\" stroke-width=\"1\"/>\n"));
