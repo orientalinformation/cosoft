1c39714b993759f471207a157c54d48da156d39e
Oriental Tran <dongtp@dfm-engineering.com>
2018-07-20 10:21:02 +0700

    add function get all study

--- app/Http/Controllers/Api1/Chaining.php
+++ app/Http/Controllers/Api1/Chaining.php
@@ -30,78 +30,133 @@ class Chaining extends Controller
 
     public function __construct(\Laravel\Lumen\Application $app)
     {
-        $this->app = $app;
-        $this->auth = $app['Illuminate\\Contracts\\Auth\\Factory'];
-        $this->request = $app['Illuminate\\Http\\Request'];
-        $this->equip = $app['App\\Cryosoft\\EquipmentsService'];
-        $this->kernel = $app['App\\Kernel\\KernelService'];
-        $this->units = $app['App\\Cryosoft\\UnitsService'];
-        $this->values = $app['App\\Cryosoft\\ValueListService'];
-        $this->converts = $app['App\\Cryosoft\\UnitsConverterService'];
-
+      $this->app = $app;
+      $this->auth = $app['Illuminate\\Contracts\\Auth\\Factory'];
+      $this->request = $app['Illuminate\\Http\\Request'];
+      $this->equip = $app['App\\Cryosoft\\EquipmentsService'];
+      $this->kernel = $app['App\\Kernel\\KernelService'];
+      $this->units = $app['App\\Cryosoft\\UnitsService'];
+      $this->values = $app['App\\Cryosoft\\ValueListService'];
+      $this->converts = $app['App\\Cryosoft\\UnitsConverterService'];
     }
 
     public function getAllChaining($id) 
     {
-        $study = Study::findOrFail($id);
+      $study = Study::findOrFail($id);
 
-        $chaining = $chain = [];
+      $chaining = $chain = [];
 
-        $chain['studyName'] = $study->STUDY_NAME;
-        $chain['ID_STUDY'] = $id;
-        $chain['active'] = 1;
-        array_push($chaining, $chain);
+      $chain['studyName'] = $study->STUDY_NAME;
+      $chain['ID_STUDY'] = $id;
+      $chain['active'] = 1;
+      array_push($chaining, $chain);
 
-        $parents  = $this->getParentChaining($id);
-        if (count($parents) > 0) {
-          foreach ($parents as $parent) {
-            array_push($chaining, $parent);
-          }
+      $parents  = $this->getParentChaining($id);
+      if (count($parents) > 0) {
+        foreach ($parents as $parent) {
+          array_push($chaining, $parent);
         }
+      }
 
-        $childrens = $this->getChildChaining($id);
-        if (count($childrens) > 0) {
-          foreach ($childrens as $child) {
-            array_push($chaining, $child);
-          }
+      $childrens = $this->getChildChaining($id);
+      if (count($childrens) > 0) {
+        foreach ($childrens as $child) {
+          array_push($chaining, $child);
         }
+      }
 
-        return $chaining;
+      return $chaining;
+    }
+
+    public function getOverViewChaining($id)
+    {
+      $study = Study::findOrFail($id);
+
+      $idStudies = $chainings = [];
+
+      array_push($idStudies, intval($id));
+
+      $parents  = $this->getParentOverviewChaining($id);
+      if (count($parents) > 0) {
+        foreach ($parents as $parent) {
+          array_push($idStudies, $parent);
+        }
+      }
+
+      $childrens = $this->getChildOverviewChaining($id);
+      if (count($childrens) > 0) {
+        foreach ($childrens as $child) {
+          array_push($idStudies, $child);
+        }
+      }
+      sort($idStudies);
+
+      return $idStudies;
     }
 
     private function getParentChaining($idStudy, $chaining = [])
     {
-        $study = Study::findOrFail($idStudy);
-        $parent = null;
-        if ($study->PARENT_ID != 0) {
-            $parent = Study::findOrFail($study->PARENT_ID);
-            $chain['studyName'] = $parent->STUDY_NAME;
-            $chain['ID_STUDY'] = $parent->ID_STUDY;
-            $chain['active'] = 0;
-            array_push($chaining, $chain);
+      $study = Study::findOrFail($idStudy);
+      $parent = null;
+      if ($study->PARENT_ID != 0) {
+          $parent = Study::findOrFail($study->PARENT_ID);
+          $chain['studyName'] = $parent->STUDY_NAME;
+          $chain['ID_STUDY'] = $parent->ID_STUDY;
+          $chain['active'] = 0;
+          array_push($chaining, $chain);
 
-            $chaining = $this->getParentChaining($study->PARENT_ID, $chaining);
-        }
+          $chaining = $this->getParentChaining($study->PARENT_ID, $chaining);
+      }
 
-        return $chaining;
+      return $chaining;
+    }
+
+    private function getParentOverviewChaining($idStudy, $chaining = []) 
+    {
+      $study = Study::findOrFail($idStudy);
+      $parent = null;
+      if ($study->PARENT_ID != 0) {
+        $parent = Study::findOrFail($study->PARENT_ID);
+
+        array_push($chaining, $parent->ID_STUDY);
+
+        $chaining = $this->getParentOverviewChaining($study->PARENT_ID, $chaining);
+      }
+
+      return $chaining;
     }
 
     private function getChildChaining($idStudy, $chaining = [])
     {
-        $study = Study::findOrFail($idStudy);
-        $children = null;
-        if ($study->HAS_CHILD != 0) {
-            $children = Study::where('PARENT_ID', $study->ID_STUDY)->first();
-            if (count($children) > 0) {
-                array_push($chaining, [
-                    'studyName' => $children->STUDY_NAME,
-                    'ID_STUDY' => $children->ID_STUDY,
-                    'active' => 0
-                ]);
+      $study = Study::findOrFail($idStudy);
+      $children = null;
+      if ($study->HAS_CHILD != 0) {
+          $children = Study::where('PARENT_ID', $study->ID_STUDY)->first();
+          if (count($children) > 0) {
+              array_push($chaining, [
+                  'studyName' => $children->STUDY_NAME,
+                  'ID_STUDY' => $children->ID_STUDY,
+                  'active' => 0
+              ]);
 
-                $chaining = $this->getChildChaining($children->ID_STUDY, $chaining);
-            }
-        }
-        return $chaining;
+              $chaining = $this->getChildChaining($children->ID_STUDY, $chaining);
+          }
+      }
+      return $chaining;
+    }
+
+    private function getChildOverviewChaining($idStudy, $chaining = [])
+    {
+      $study = Study::findOrFail($idStudy);
+      $children = null;
+      if ($study->HAS_CHILD != 0) {
+          $children = Study::where('PARENT_ID', $study->ID_STUDY)->first();
+          if (count($children) > 0) {
+              array_push($chaining, $children->ID_STUDY);
+
+              $chaining = $this->getChildOverviewChaining($children->ID_STUDY, $chaining);
+          }
+      }
+      return $chaining;
     }
 }
--- routes/api_chaining.php
+++ routes/api_chaining.php
@@ -7,3 +7,11 @@
  * Output-Formats: [application/json]
  */
 $router->GET('/api/v1/chaining/{id}', 'Api1\\Chaining@getAllChaining');
+
+/**
+ * GET all chaining
+ * Summary: 
+ * Notes: get head balance result
+ * Output-Formats: [application/json]
+ */
+$router->GET('/api/v1/chaining/overview/{id}', 'Api1\\Chaining@getOverViewChaining');
