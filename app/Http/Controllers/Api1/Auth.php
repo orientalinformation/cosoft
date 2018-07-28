c4a20a688835a381c321ec7fece5f54a748f5697
Oriental Tran <dongtp@dfm-engineering.com>
2018-07-28 15:42:00 +0700

    add check user already connected

--- app/Http/Controllers/Api1/Auth.php
+++ app/Http/Controllers/Api1/Auth.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\JWTAuth;
+use App\Models\Connection;
+use Carbon\Carbon;
 
 
 class Auth extends Controller
@@ -33,28 +35,36 @@ class Auth extends Controller
         ]);
 
         try {
-
             if (! $token = $this->jwt->attempt($request->only('username', 'password'))) {
-                return response()->json(['user_not_found'], 404);
+                return response()->json(['Username or Password is incorrect'], 404);
             }
-
         } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-
             return response()->json(['token_expired'], 500);
-
         } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-
             return response()->json(['token_invalid'], 500);
-
         } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
-
             return response()->json(['token_absent' => $e->getMessage()], 500);
-
         }
+
+        $connection = Connection::where('ID_USER', $this->auth->user()->ID_USER)
+                        ->where('TYPE_DISCONNECTION', 0)->first();
+        if ($connection) {
+            return response()->json(['User already connected'], 404);
+        }
+
+        $current = Carbon::now();
+        $current->timezone = 'Asia/Ho_Chi_Minh';
 
         $user = $this->auth->user();
         if ($user) {
             $user->USERMAIL = null;
+
+            $connection = new Connection();
+            $connection->ID_USER = $user->ID_USER;
+            $connection->DATE_CONNECTION = $current->toDateTimeString();
+            $connection->DATE_DISCONNECTION = null;
+            $connection->TYPE_DISCONNECTION = 0;
+            $connection->save();
         }
 
         return response()->json(compact('token','user'));
@@ -62,6 +72,16 @@ class Auth extends Controller
 
     public function logout(Request $request)
     {
-        
+        $current = Carbon::now();
+        $current->timezone = 'Asia/Ho_Chi_Minh';
+
+        $connections = Connection::where('ID_USER', $this->auth->user()->ID_USER)
+                        ->where('TYPE_DISCONNECTION', 0)->get();
+
+        foreach ($connections as $connection) {
+            $connection->DATE_DISCONNECTION = $current->toDateTimeString();
+            $connection->TYPE_DISCONNECTION = 2;
+            $connection->update();
+        }
     }
 }
